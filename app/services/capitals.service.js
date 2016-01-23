123776e219a8a5d424f7b46c2beca2a79955ad58
jsebfranck <jsebfranck@gmail.com>
2016-01-23 09:53:35 +0100

    Instructions finales atelier 1

--- app/services/capitals.service.js
+++ app/services/capitals.service.js
@@ -1,12 +1,15 @@
 
 
+// Renvoie la liste de toutes les capitales
+// Exemple de capitale : { "country": "France", "city": "Paris" }
 var getCapitals = function(cb) {
     var capitals = require(__dirname + '/../../data/capitals.json');
     cb(null, capitals);
-}
+};
 
 exports.getCapitals = getCapitals;
 
+// Parcours la liste des capitales et renvoie la capitale qui correspond au pays passé en paramètre
 exports.getCapitalByCountry = function (country, cb) {
     getCapitals(function (err, capitals) {
         var foundCapital;
@@ -27,6 +30,8 @@ exports.getCapitalByCountry = function (country, cb) {
     });
 };
 
+// Renvoie une liste de capitales aléatoires. La taille attendue de la liste correspond au paramètre "count".
+// Pour générer un nombre entier aléatoire on peut utiliser les fonctions Math.random et Math.floor.
 exports.getRandomCapitals = function (count, cb) {
     getCapitals(function (err, capitals) {
 
--- app/services/quizz.service.js
+++ app/services/quizz.service.js
@@ -1,6 +1,8 @@
 
 var capitalsService = require('./capitals.service');
 
+// Vérifie que le choix de l'utilisateur est correct
+// Exemple de résultat : { isCorrect: <booleanValue> }
 exports.answerQuestion = function (answer, cb) {
     capitalsService.getCapitalByCountry(answer.getCountry(), function (err, expectedCapital) {
         var isCorrect = !err && answer.getUserChoice() === expectedCapital.city;
@@ -11,12 +13,13 @@ exports.answerQuestion = function (answer, cb) {
     });
 };
 
+// Récupère la liste des capitales à l'aide du service des capitales
 exports.getCapitals = function (cb) {
-    capitalsService.getCapitals(function (err, capitals) {
-        cb(null, capitals);
-    });
+    capitalsService.getCapitals(cb);
 };
 
+// Génère une nouvelle Question avec 6 choix possibles de réponse
+// Exemple de résultat : { country: 'France', cities: [ 'Paris', 'Berlin', 'Madrid', 'Londres', 'Rome', 'Bruxelles']}
 exports.getNewQuestion = function (cb) {
 
     capitalsService.getRandomCapitals(6, function (err, capitals) {
