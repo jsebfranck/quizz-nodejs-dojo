88c0b87ea53ad523c386c20d520c96fc22ccbbd3
jsebfranck <jsebfranck@gmail.com>
2016-01-23 12:27:11 +0100

    Step3

--- app/services/quizz.service.js
+++ app/services/quizz.service.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var capitalsService = require('./capitals.service'),
-    scoreService = require('./score.service'),
     arrayUtil = require('./array.util');
 
 // Vérifie que le choix de l'utilisateur est correct
@@ -10,12 +9,7 @@ exports.answerQuestion = function (answer, cb) {
     capitalsService.getCapitalByCountry(answer.getCountry(), function (err, expectedCapital) {
         var isCorrect = !err && answer.getUserChoice() === expectedCapital.city;
 
-        scoreService.newAnswer(answer.getLogin(), isCorrect, function (err, score) {
-            cb(null, {
-                userScore: score,
-                isCorrect: isCorrect
-            });
-        });
+        //TODO
     });
 };
 
--- app/services/score.service.js
+++ app/services/score.service.js
@@ -1,24 +1,8 @@
 'use strict';
 
-var score = {};
-
 // Enregistre le score de l'utilisateur
 // Puis répond avec le score de l'utilisateur sour la forme :
 //   { questions: <nombre de questions déjà répondues>, success: <nombre de questions répondues avec succès>}
 exports.newAnswer = function (login, isCorrect, cb) {
 
-    if (!score[login]) {
-        score[login] = {
-            questions: 0,
-            success: 0
-        };
-    };
-
-    score[login].questions++;
-
-    if (isCorrect) {
-        score[login].success++;
-    }
-
-    cb(null, score[login]);
 };
